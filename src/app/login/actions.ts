308f3396743731ecbe288842cc01355b1c126012
jochemwhite <j.vanderwit@amrio.nl>
2024-05-06 04:56:59 +0200

    chore: Update Twitch login scopes in login actions

--- src/app/login/actions.ts
+++ src/app/login/actions.ts
@@ -4,15 +4,24 @@ import { redirect } from "next/navigation";
 import { createClient } from "@/utils/supabase/server";
 import { headers } from "next/headers";
 
+const TWITCH_SCOPES = [
+  "channel:read:editors",
+  "channel:manage:redemptions",
+  "channel:read:subscriptions",
+  "channel:read:vips",
+  "moderation:read",
+  "moderator:read:followers",
+];
+
 export async function login() {
   const supabase = createClient();
   const origin = headers().get("origin");
 
-
   const { error, data } = await supabase.auth.signInWithOAuth({
     provider: "twitch",
     options: {
-      redirectTo: `${origin}/auth/callback`
+      redirectTo: `${origin}/auth/callback`,
+      scopes: TWITCH_SCOPES.join(" "),
     },
   });
 
