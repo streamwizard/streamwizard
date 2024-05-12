4240a6f845fb31d6d20074818aa56b5e7a3d3886
jochemwhite <j.vanderwit@amrio.nl>
2024-05-12 12:07:52 +0200

    added a the function to seach for songs SSR, and added a table to view the banned songs

---
+++ src/config/axios/spotify-web-api.ts
@@ -0,0 +1,97 @@
+"use server";
+
+import { supabaseAdmin } from "@/utils/supabase/admin";
+import axios from "axios";
+import type { AxiosRequestConfig } from "axios";
+
+const SpotifyWebAPi = axios.create({
+  baseURL: "https://api.spotify.com/v1/",
+  headers: {
+    Accept: "application/json",
+  },
+});
+
+SpotifyWebAPi.interceptors.request.use(
+  (config) => {
+    // Assuming you have a method to get the current token...]
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+//twitch response interceptor
+SpotifyWebAPi.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  //handle response error
+  async function (error) {
+    //originalRequest
+    const originalRequest = error.config;
+
+    //if the error status = 401 we update the token and retry
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      //get the channel from the request
+      const channelID = error.response?.config.broadcasterID;
+
+      const { data, error: DBerror } = await supabaseAdmin
+        .from("twitch_integration")
+        .select("refresh_token")
+        .eq("broadcaster_id", channelID)
+        .single();
+      if (DBerror) {
+        console.log("Tokens not found");
+        return;
+      }
+
+      //fetch the new accessToken and update the tokens
+      const newToken = await RefreshToken(data.refresh_token, channelID);
+
+      if (!newToken) {
+        console.log("Error refreshing token");
+        return;
+      }
+
+      //update the headers for the new request
+      originalRequest.headers["Authorization"] = "Bearer " + newToken;
+
+      //make the new request
+      const res = SpotifyWebAPi(originalRequest);
+
+      return res;
+    }
+    return Promise.reject(error);
+  }
+);
+
+export { SpotifyWebAPi };
+
+async function RefreshToken(refreshToken: string, broadcaster_id: number): Promise<string | null> {
+  console.log("refreshing token");
+  try {
+    const res = await axios.post(
+      `https://id.twitch.tv/oauth2/token?client_id=${process.env.NEXT_PUBLIC_TWITCH_CLIENT_ID}&client_secret=${process.env.TWITCH_CLIENT_SECRET}&grant_type=refresh_token&refresh_token=${refreshToken}`
+    );
+
+    const { error } = await supabaseAdmin
+      .from("twitch_integration")
+      .update({ access_token: res.data.access_token, refresh_token: res.data.refresh_token })
+      .eq("broadcaster_id", broadcaster_id);
+
+      if(error){
+        console.log("error updating tokens");
+        return null;
+      }
+
+    return res.data.access_token;
+  } catch (error) {
+    console.error("error refreshing token");
+    console.log(error);
+    return null;
+  }
+}
