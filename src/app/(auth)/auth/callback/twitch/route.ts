0d4a9894b55e58aea94264d73f895921784263f5
Jochemwhite <j.vanderwit@amrio.nl>
2024-11-11 16:15:24 +0100

    coolify log

--- src/app/(auth)/auth/callback/twitch/route.ts
+++ src/app/(auth)/auth/callback/twitch/route.ts
@@ -8,9 +8,9 @@ export async function GET(request: Request) {
   console.log("twitch callback called");
 
 
-
-
   const { searchParams, origin } = new URL(request.url);
+
+  console.log(`origin in callback: ${origin}`);
 
   const code = searchParams.get("code");
   // if "next" is in param, use it as the redirect URL
@@ -55,12 +55,15 @@ export async function GET(request: Request) {
 
     if (!error) {
       const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer
+
+      console.log(`forwardedHost: ${forwardedHost}`);
+
       const isLocalEnv = process.env.NODE_ENV === "development";
       if (isLocalEnv) {
         // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
         return NextResponse.redirect(`${origin}${next}`);
       } else if (forwardedHost) {
-        return NextResponse.redirect(`https://${forwardedHost}${next}`);
+        return NextResponse.redirect(`https://${origin}${next}`);
       } else {
         return NextResponse.redirect(`${origin}${next}`);
       }
