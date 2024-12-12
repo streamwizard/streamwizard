00832546ebb42ab3889d1b088d250468552bebfc
Jochemwhite <j.vanderwit@amrio.nl>
2024-12-12 23:20:18 +0100

    added redirect

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,5 +1,9 @@
+import { redirect } from "next/navigation";
 
 export default function ClipsPage({ searchParams }: { searchParams: Promise<{ [key: string]: string | undefined }> }) {
+  redirect("/dashboard/clips");
+
+
   return (
     <></>
   );
