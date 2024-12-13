7a3a74b3a739834497177771b136ae8d0faef21f
Jochemwhite <j.vanderwit@amrio.nl>
2024-12-13 03:56:02 +0100

    changed the BG color of the sidebar

--- src/components/nav/dashboardNav.tsx
+++ src/components/nav/dashboardNav.tsx
@@ -4,7 +4,7 @@ import Notifications from './notifications'
 
 export function DashboardNav() {
   return (
-    <section className="flex h-[60px] select-none items-center justify-end px-6 md:border-b md:border-border md:bg-border/10">
+    <section className="flex h-[60px] select-none items-center justify-end px-6 md:border-b md:border-border bg-sidebar">
       <div className="flex w-full items-center justify-between gap-5 p-4 text-sm">
         {/* <CommandMenu /> */}
 
