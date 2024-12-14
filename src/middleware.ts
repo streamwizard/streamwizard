8c207b033a170d8f75fcf0c4c1fc32739fe399a0
Jochemwhite <j.vanderwit@amrio.nl>
2024-12-14 02:31:33 +0100

    added a log for coolify

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,6 +8,8 @@ export async function middleware(request: NextRequest) {
   if(env.NODE_ENV === "test") {
     const clientIP = (await headers()).get("x-forwarded-for") || null;
 
+    console.log(clientIP);
+
     if (!clientIP) return NextResponse.redirect(env.NEXT_PUBLIC_BASE_URL);
   
     const { data, error } = await supabaseAdmin.from("dev_access_ips").select("*").eq("ip_address", clientIP).eq("is_active", true).single();
