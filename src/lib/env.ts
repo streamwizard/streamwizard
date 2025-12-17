380343d8d6e66a34a17b1bd603bb3131df0db230
Jochemwhite <j.vanderwit@amrio.nl>
2025-12-17 14:49:39 +0100

    feat: integrate Sentry for error tracking in Next.js application and update search bar component for improved state management

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -18,11 +18,14 @@ export const env = createEnv({
     NEXT_PUBLIC_BASE_URL: z.string().min(1),
 
     NEXT_PUBLIC_TWITCH_CLIENT_ID: z.string().min(1),
+
+    NEXT_PUBLIC_SENTRY_DSN: z.string().url(),
   },
   experimental__runtimeEnv: {
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     NEXT_PUBLIC_TWITCH_CLIENT_ID: process.env.NEXT_PUBLIC_TWITCH_CLIENT_ID,
     NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+    NEXT_PUBLIC_SENTRY_DSN: process.env.NEXT_PUBLIC_SENTRY_DSN,
   },
 });
