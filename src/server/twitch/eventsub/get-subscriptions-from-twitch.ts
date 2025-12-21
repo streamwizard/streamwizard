75e85daa9eb00a53a9619f22c206a2c0a72c878b
Jochemwhite <j.vanderwit@amrio.nl>
2025-12-21 22:12:21 +0100

    chore: add shadcn command configuration to mcp.json, remove unused create-app-token.ts file, and refactor Twitch event subscription functions to use centralized TwitchAppAPI for improved API calls

--- src/server/twitch/eventsub/get-subscriptions-from-twitch.ts
+++ src/server/twitch/eventsub/get-subscriptions-from-twitch.ts
@@ -1,17 +1,8 @@
-import { env } from "@/lib/env";
-import { GetEventSubSubscriptionsResponse } from "@/types/twitch";
-import axios from "axios";
+  import { GetEventSubSubscriptionsResponse } from "@/types/twitch";
+import { TwitchAppAPI } from "@/server/axios/twitch-app-token";
 
 export default async function getSubscriptionFromTwitch(twitchUserId: string) {
-  const res = await axios.get<GetEventSubSubscriptionsResponse>("https://api.twitch.tv/helix/eventsub/subscriptions", {
-    headers: {
-      "Client-Id": env.NEXT_PUBLIC_TWITCH_CLIENT_ID,
-      Authorization: `Bearer ${env.TWITCH_APP_TOKEN}`,
-    },
-    params: {
-      user_id: twitchUserId,
-    },
-  });
+  const res = await TwitchAppAPI.get<GetEventSubSubscriptionsResponse>(`/eventsub/subscriptions?user_id=${twitchUserId}`);
 
   return res.data.data;
 }
