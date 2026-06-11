ea249cc50d2883e28fe179019fca8f3b384e1b92
jochemwhite <j.vanderwit@amrio.nl>
2026-06-11 12:04:55 +0200

    refactor: improve layout and responsiveness of login page and form
    - Adjusted the layout of the login page to use a grid system for better responsiveness.
    - Updated the login form controls to utilize full width for improved usability.
    - Enhanced styling for a more cohesive design across different screen sizes.

--- apps/web-streamwizard/src/app/(auth)/login/page.tsx
+++ apps/web-streamwizard/src/app/(auth)/login/page.tsx
@@ -13,8 +13,8 @@ export const metadata: Metadata = {
 
 export default function LoginPage() {
   return (
-    <div className=" relative hidden  flex-col items-center h-screen justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-      <div className="relative  h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
+    <div className="relative grid min-h-screen flex-col items-center justify-center lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
         <div className="absolute inset-0 bg-zinc-900" />
         <div className="z-50 h-full flex justify-center items-center flex-col relative">
           <Image
@@ -46,8 +46,8 @@ export default function LoginPage() {
           </blockquote>
         </div>
       </div>
-      <div className="lg:p-8">
-        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+      <div className="px-4 py-8 sm:px-8">
+        <div className="mx-auto flex w-full max-w-sm flex-col justify-center space-y-6">
           <div className="flex flex-col space-y-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">Login</h1>
             <p className="text-sm text-muted-foreground">
